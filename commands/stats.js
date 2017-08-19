43046690b92da0ac4f462ec3063c055b2c2c91d7
Dave R <davetheprogrammer2@gmail.com>
2017-08-19 17:55:35 +0100

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -1,4 +1,4 @@
 exports.run = (client, msg, args) => {
-    msg.channel.send(`I am on **${client.guilds.size}** with **${client.users.size}** users!`)   
+    msg.channel.send(`I am on **${client.guilds.size}** guilds with **${client.users.size}** users!`)   
 
 }
