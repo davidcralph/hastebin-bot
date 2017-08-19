089d00122b62f68f798edfbf00ee909275dbffef
Dave R <davetheprogrammer2@gmail.com>
2017-08-19 17:56:20 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,4 @@
 exports.run = (client, msg, args) => {
-    msg.channel.send("Here are my commands: ```help (shows this command), ping (tells you the ping), stats (tells you the stats, haste (posts data to hastebin, the last word you type will be the file format!) and invite (gives you bot invite link)```") 
+    msg.channel.send("Here are my commands: ```help (shows this command), ping (tells you the ping), stats (tells you the stats, haste (posts data to hastebin, the last word you type will be the file format!), cat (requested by untocodes) and invite (gives you bot invite link)```") 
 
 }
