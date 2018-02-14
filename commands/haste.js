ae887624e634835b53853f4d0df9a54d17f65e49
Dave R <14052956+ohlookitsderpy@users.noreply.github.com>
2018-02-14 17:38:44 +0000

    Update haste.js

--- commands/haste.js
+++ commands/haste.js
@@ -1,9 +1,7 @@
 const hastebin = require('hastebin-gen');
 exports.run = (client, msg, args) => {
-	    let haste = args.slice(0).join(" ")
+	let haste = args.slice(0).join(" ")
         let type = args.slice(1).join(" ")
         if (!args[0]) { return msg.channel.send(":x: What do you want to post to Hastebin?") }
-        hastebin(haste, type).then(r => {
-            msg.channel.send(":white_check_mark: Posted text to Hastebin at this URL: " + r);
-        }).catch(console.error);
+        hastebin(haste, type).then(r => { msg.channel.send(":white_check_mark: Posted text to Hastebin at this URL: " + r); }).catch(console.error);
 }
