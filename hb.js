214a7a727c9b00b734ef5298ae41752ad8181ed3
Dave R <14052956+ohlookitsderpy@users.noreply.github.com>
2018-02-14 17:37:56 +0000

    Update hb.js

--- hb.js
+++ hb.js
@@ -1,25 +1,7 @@
 const { Client } = require("discord.js")
-const client = new Client({
-    disableEveryone: true
-});
-const fs = require("fs");
+const client = new Client({ disableEveryone: true, autoReconnect: true });
 const config = require("./config.json");
-const snekfetch = require('snekfetch');
-
-client.on('ready', () => {
-    console.log('beep boop')
-});
-client.on('guildCreate', (guild) => {
-    guild.defaultChannel.send("Hello, I am Hastebin Bot and I post data to Hastebin. Do **hb!help** for my commands!")
-    snekfetch
-       .post(`https://discordbots.org/api/bots/${client.user.id}/stats`)
-       .set(`Authorization`, ) // Add a key ;)
-       .send({
-        server_count: client.guilds.size
-       })
-       .then(console.log(`[oliyBots] Posted O:`))
-});
-
+client.on('ready', () => { console.log('beep boop'); });
 client.on("message", message => {
   if (message.author.bot) return;
   if(message.content.indexOf(config.prefix) !== 0) return;
@@ -28,9 +10,6 @@ client.on("message", message => {
   try {
     let commandFile = require(`./commands/${command}.js`);
     commandFile.run(client, message, args);
-  } catch (err) {
-    console.error(err);
-  }
+  } catch (err) { console.error(err); }
 });
-
 client.login(config.token);
