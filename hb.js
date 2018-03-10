3842be5e58705d920013c9018c8ca3a0688d008f
Dave R <14052956+ohlookitsderpy@users.noreply.github.com>
2018-03-10 08:22:00 +0000

    no u

--- hb.js
+++ hb.js
@@ -1,10 +1,12 @@
 const { Client } = require("discord.js")
 const client = new Client({ disableEveryone: true, autoReconnect: true });
 const config = require("./config.json");
+
 client.on('ready', () => { console.log('beep boop'); });
+
 client.on("message", message => {
   if (message.author.bot) return;
-  if(message.content.indexOf(config.prefix) !== 0) return;
+  if (message.content.indexOf(config.prefix) !== 0) return;
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   try {
@@ -12,4 +14,5 @@ client.on("message", message => {
     commandFile.run(client, message, args);
   } catch (err) { console.error(err); }
 });
+
 client.login(config.token);
