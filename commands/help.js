3f0622ef7f589366bb8c36dcd65a365bd18d72bb
Dave R <14052956+ohlookitsderpy@users.noreply.github.com>
2018-03-10 08:22:41 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,10 +2,10 @@ const { RichEmbed } = require('discord.js');
 exports.run = (client, msg, args) => {
   const embed = new RichEmbed()
      .setTitle("Commands:")
-     .addField("help","This embed.")
-     .addField("ping","Pong.")
+     .addField("help","This message.")
+     .addField("ping","Pong!")
      .addField("stats","Get the status of this bot.")
-     .addField("haste","Add code to hastebin.")
+     .addField("haste","Upload text to Hastebin!")
      .addField("invite","Invite me to your server.")
      .setTimestamp()
      .setFooter(`Hastebin Bot; Made by ohlookitsderpy`)
