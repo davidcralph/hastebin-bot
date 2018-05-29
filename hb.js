1fa99275c0de149140271c99eb4b700bcec7ce10
Dave R <14052956+ohlookitsderpy@users.noreply.github.com>
2018-05-29 09:09:43 +0100

    new cmd handler

--- hb.js
+++ hb.js
@@ -1,18 +1,40 @@
+/*
+  _    _           _       _     _         ____        _   
+ | |  | |         | |     | |   (_)       |  _ \      | |  
+ | |__| | __ _ ___| |_ ___| |__  _ _ __   | |_) | ___ | |_ 
+ |  __  |/ _` / __| __/ _ \ '_ \| | '_ \  |  _ < / _ \| __|
+ | |  | | (_| \__ \ ||  __/ |_) | | | | | | |_) | (_) | |_ 
+ |_|  |_|\__,_|___/\__\___|_.__/|_|_| |_| |____/ \___/ \__|
+                                                          
+*/                                                           
 const { Client } = require("discord.js")
+const Enmap = require("enmap");
 const client = new Client({ disableEveryone: true, autoReconnect: true });
 const config = require("./config.json");
 
-client.on('ready', () => { console.log('beep boop'); });
+client.config = config;
 
-client.on("message", message => {
-  if (message.author.bot) return;
-  if (message.content.indexOf(config.prefix) !== 0) return;
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-  try {
-    let commandFile = require(`./commands/${command}.js`);
-    commandFile.run(client, message, args);
-  } catch (err) { console.error(err); }
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    console.log(`loading ${eventName}`);
+    client.on(eventName, event.bind(null, client));
+  });
+});
+
+client.commands = new Enmap();
+
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    console.log(`loading ${commandName}`);
+    client.commands.set(commandName, props);
+  });
 });
 
 client.login(config.token);
