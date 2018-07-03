15838d8831059113217773b5896f0ae80b4e8d71
Dave R <davetheprogrammer2@gmail.com>
2018-07-03 11:53:06 +0100

    improved

--- commands/haste.js
+++ commands/haste.js
@@ -1,8 +1,8 @@
 const snekfetch = require("snekfetch");
 
 exports.run = (client, msg, args) => {
-         if (!args[0]) { return msg.channel.send(":x: What do you want to post to Hastebin?") }
-	 snekfetch.post("https://hastebin.com/documents").send(args.slice(0).join(" ")).then(body => {
-            msg.channel.send(":white_check_mark: Posted text to Hastebin at this URL: https://hastebin.com/" + body.body.key);
+         if (!args[0]) return msg.channel.send(":x: What do you want to post to Hastebin?");
+	 snekfetch.post(`${client.config.hasteurl}/documents`).send(args.slice(0).join(" ")).then(body => {
+            msg.channel.send(`:white_check_mark: Posted text to Hastebin at this URL: ${client.config.hasteurl}/${body.body.key}`);
         });
 }
