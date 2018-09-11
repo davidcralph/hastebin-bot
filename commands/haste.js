2dd3a576fb104714fcadc75228d9cb229344cfcd
Dave R <davetheprogrammer2@gmail.com>
2018-09-11 15:01:29 +0100

    Update haste.js

--- commands/haste.js
+++ commands/haste.js
@@ -1,8 +1,8 @@
-const snekfetch = require("snekfetch");
+const fetch = require("node-superfetch");
 
 exports.run = (client, msg, args) => {
          if (!args[0]) return msg.channel.send(":x: What do you want to post to Hastebin?");
-	 snekfetch.post(`${client.config.hasteurl}/documents`).send(args.slice(0).join(" ")).then(body => {
+	 fetch.post(`${client.config.hasteurl}/documents`).send(args.slice(0).join(" ")).then(body => {
             msg.channel.send(`:white_check_mark: Posted text to Hastebin at this URL: ${client.config.hasteurl}/${body.body.key}`);
         });
 }
