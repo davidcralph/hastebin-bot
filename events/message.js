a34ff97225ccb26b34e14645199a94723fc562ef
Dave R <davetheprogrammer2@gmail.com>
2018-09-11 17:07:07 +0100

    cool code
    actually it succ but ok

--- events/message.js
+++ events/message.js
@@ -1,10 +1,9 @@
 module.exports = (client, msg) => {
-  if (msg.author.bot) return;
-  if (!msg.guild) return;
-  if (msg.content.indexOf(client.config.prefix) !== 0) return;
-  const args = msg.content.slice(client.config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-  const cmd = client.commands.get(command);
-  if (!cmd) return;
-  cmd.run(client, msg, args);
+ if (msg.author.bot) return;
+ if (msg.content.indexOf(client.config.prefix) !== 0) return;
+ const args = msg.content.slice(client.config.prefix.length).trim().split(/ +/g),
+       command = args.shift().toLowerCase(),
+       cmd = client.commands.get(command);
+ if (!cmd) return;
+ cmd.run(client, msg, args);
 };
