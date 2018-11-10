05dcefef09535dea116a590bb7cc9defcbec1323
Dave R <davetheprogrammer2@gmail.com>
2018-11-10 14:11:16 +0000

    make it a bit more readable

--- commands/haste.js
+++ commands/haste.js
@@ -2,9 +2,15 @@ const fetch = require("node-fetch");
 
 exports.run = (client, msg, args) => {
     if (!args[0]) return msg.channel.send(":x: I can't post nothing to Hastebin!");
-    fetch(`https://hastebin.com/documents`, { method: 'POST', body: args.slice(0).join(" "), headers: { 'Content-Type': 'application/json' } })
-        .then(res => res.json())
-        .then(json => {
+    fetch(`https://hastebin.com/documents`, { 
+        method: 'POST',
+        body: args.slice(0).join(" "), 
+        headers: { 
+            'Content-Type': 'application/json' 
+        } 
+    })
+      .then(res => res.json())
+      .then(json => {
             msg.channel.send(`:white_check_mark: Posted text to Hastebin at this URL: https://hastebin.com/${json.key}`);
-        });
+      });
 }
