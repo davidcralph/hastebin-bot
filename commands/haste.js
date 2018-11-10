7339e0a2747a272ba0833d90ab749b20c3704a9f
Dave R <davetheprogrammer2@gmail.com>
2018-11-10 14:06:05 +0000

    wasn't cool enough

--- commands/haste.js
+++ commands/haste.js
@@ -1,8 +1,10 @@
-const { post } = require("node-superfetch");
+const fetch = require("node-fetch");
 
-exports.run = async(client, msg, args) => {
+exports.run = (client, msg, args) => {
     if (!args[0]) return msg.channel.send(":x: I can't post nothing to Hastebin!");
-    const res = await post(`${client.config.hasteurl}/documents`)
-                     .send(args.slice(0).join(" "));
-    msg.channel.send(`:white_check_mark: Posted text to Hastebin at this URL: ${client.config.hasteurl}/${res.body.key}`);
+    fetch(`https://hastebin.com/documents`, { method: 'POST', body: args.slice(0).join(" "), headers: { 'Content-Type': 'application/json' } })
+        .then(res => res.json())
+        .then(json => {
+            msg.channel.send(`:white_check_mark: Posted text to Hastebin at this URL: https://hastebin.com/${json.key}`);
+        });
 }
