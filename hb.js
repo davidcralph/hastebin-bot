d2b67d72045d3d7fb0b00c3ae8c408beeeabd07a
Dave R <davetheprogrammer2@gmail.com>
2018-11-10 13:27:47 +0000

    Update hb.js

--- hb.js
+++ hb.js
@@ -8,34 +8,34 @@
   Made by ohlookitsderpy, pull requests made by other people!
   MIT License TM                                                        
 */                                                           
-const Discord = require("discord.js"),
-      client     = new Discord.Client({ disableEveryone: true, autoReconnect: true }),
-      config     = require("./config.json"),
-      log        = require("umi-log");
+const Discord = require("discord.js");
+const client  = new Discord.Client({ 
+  disableEveryone: true, 
+  autoReconnect: true 
+});
 
 fs.readdir("./events/", (err, files) => {
-  if (err) return log.error('[Discord] ' + err);
+  if (err) return console.log(err);
   files.forEach(file => {
     const event = require(`./events/${file}`);
     let eventName = file.split(".")[0];
-    log.info(`[Discord] Loading ${eventName}`);
+    console.log(`Loading ${eventName}`);
     client.on(eventName, event.bind(null, client));
   });
 });
 
 client.commands = new Discord.Collection();
-client.config = config;
-client.log = log;
+client.config = require("./config.json");
 
 fs.readdir("./commands/", (err, files) => {
-  if (err) return log.error('[Discord] ' + err);
+  if (err) return console.log(err);
   files.forEach(file => {
     if (!file.endsWith(".js")) return;
     let props = require(`./commands/${file}`);
     let commandName = file.split(".")[0];
-    log.info(`[Discord] Loading ${commandName}`);
+    console.log(`Loading ${commandName}`);
     client.commands.set(commandName, props);
   });
 });
 
-client.login(config.token);
+client.login(client.config.token);
