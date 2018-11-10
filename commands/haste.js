f6efd0ec2142cedd9ff0920753d6eda7de0d1caf
Dave R <davetheprogrammer2@gmail.com>
2018-11-10 13:23:35 +0000

    make it cooler

--- commands/haste.js
+++ commands/haste.js
@@ -1,8 +1,8 @@
-const fetch = require("node-superfetch");
+const { post } = require("node-superfetch");
 
-exports.run = (client, msg, args) => {
-    if (!args[0]) return msg.channel.send(":x: What do you want to post to Hastebin?");
-    fetch.post(`${client.config.hasteurl}/documents`).send(args.slice(0).join(" ")).then(body => {
-        msg.channel.send(`:white_check_mark: Posted text to Hastebin at this URL: ${client.config.hasteurl}/${body.body.key}`);
-    });
+exports.run = async(client, msg, args) => {
+    if (!args[0]) return msg.channel.send(":x: I can't post nothing to Hastebin!");
+    const res = await post(`${client.config.hasteurl}/documents`)
+                     .send(args.slice(0).join(" "));
+    msg.channel.send(`:white_check_mark: Posted text to Hastebin at this URL: ${client.config.hasteurl}/${res.body.key}`);
 }
