bc217869bd5e4e926d54de9f262df27472c6a490
Dave R <davetheprogrammer2@gmail.com>
2018-11-14 12:01:48 +0000

    don't allow bot to run in dm

--- events/message.js
+++ events/message.js
@@ -1,4 +1,5 @@
 module.exports = (client, msg) => {
+      if (!msg.guild) return;
       if (msg.author.bot) return;
       if (msg.content.indexOf(client.config.prefix) !== 0) return;
       const args = msg.content.slice(client.config.prefix.length).trim().split(/ +/g);
