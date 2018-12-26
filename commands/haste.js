019a78e19f41106b36234699ac894af820e2e4b9
David Ralph <ohlookitsderpy@protonmail.com>
2018-12-26 11:58:31 +0000

    use the proper url

--- commands/haste.js
+++ commands/haste.js
@@ -2,15 +2,11 @@ const fetch = require("node-fetch");
 
 exports.run = (client, msg, args) => {
     if (!args[0]) return msg.channel.send(":x: I can't post nothing to Hastebin!");
-    fetch(`https://hastebin.com/documents`, { 
+    fetch(`${client.config.hasteurl}/documents`, { 
         method: 'POST',
         body: args.slice(0).join(" "), 
-        headers: { 
-            'Content-Type': 'application/json' 
-        } 
+        headers: { 'Content-Type': 'application/json' } 
     })
       .then(res => res.json())
-      .then(json => {
-            msg.channel.send(`:white_check_mark: Posted text to Hastebin at this URL: https://hastebin.com/${json.key}`);
-      });
+      .then(json => { msg.channel.send(`:white_check_mark: Posted text to Hastebin at this URL: https://hastebin.com/${json.key}`); });
 }
