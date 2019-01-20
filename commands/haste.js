d22cae6978e18d58ef0c21dcbc8e76068bb92982
David Ralph <ohlookitsderpy@protonmail.com>
2019-01-20 17:16:58 +0000

    Added async (untested)

--- commands/haste.js
+++ commands/haste.js
@@ -1,12 +1,13 @@
-const fetch = require("node-fetch");
+const fetch = require('node-fetch');
 
-exports.run = (client, msg, args) => {
-    if (!args[0]) return msg.channel.send(":x: I can't post nothing to Hastebin!");
-    fetch(`${client.config.hasteurl}/documents`, { 
+exports.run = async (client, msg, args) => {
+    if (!args[0]) return msg.channel.send(':x: I can\'t post nothing to Hastebin!');
+    const options = {
         method: 'POST',
         body: args.slice(0).join(" "), 
         headers: { 'Content-Type': 'application/json' } 
-    })
-      .then(res => res.json())
-      .then(json => { msg.channel.send(`:white_check_mark: Posted text to Hastebin at this URL: ${client.config.hasteurl}/${json.key}`); });
+    }
+    let res = await fetch(`${client.config.hasteurl}/documents`, options);
+    let json = res.json();
+    msg.channel.send(`:white_check_mark: Posted text to Hastebin at this URL: ${client.config.hasteurl}/${json.key}`);
 }
