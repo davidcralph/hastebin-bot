79da5a045aeb25874f8087cfb6016b1c1b9f2284
David Ralph <ohlookitsderpy@protonmail.com>
2019-02-11 14:09:34 +0000

    oops

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,7 @@ exports.run = (client, msg, args) => {
   const embed = new MessageEmbed()
      .setTitle('Commands:')
      .addField('help','Returns this message.')
-     .addField('ping','Pong! Returns the ping')
+     .addField('ping','Pong! Returns the ping.')
      .addField('stats','Get the stats of the bot.')
      .addField('haste <text>','Upload text to Hastebin!')
      .setFooter('Made by ohlookitsderpy | https://github.com/ohlookitsderpy/hastebin-bot');
